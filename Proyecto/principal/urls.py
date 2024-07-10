f4e9117088bea455e05529e693826a9f57426fee
smYal <samu.alarcon@duocuc.cl>
2024-07-09 22:59:39 -0400

    "cambios nuevas app"

--- Proyecto/principal/urls.py
+++ Proyecto/principal/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from home.views import homeView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',homeView),
+    path('',include('home.urls')),
+  
 ]
